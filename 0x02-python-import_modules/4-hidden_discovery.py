516528b0a68eeb3ddcd7132b78eacc443bf6e659
Adetwelve <niyero10@gmail.com>
2022-12-07 16:38:32 -0800

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     import hidden_4
     for i in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+        if i[:2] != "__":
+            print(i)
