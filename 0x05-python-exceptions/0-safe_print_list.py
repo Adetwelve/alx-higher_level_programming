cce20dee45ab8edb1fec4b3a1a7c9b0204e72236
Adetwelve <niyero10@gmail.com>
2022-12-20 04:43:55 -0800

    task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    for element in my_list:
+        try:
+            if count < x:
+                print(f"{element}", end = '')
+                count += 1
+        except IndexError:
+                break;
+    print()
+    return count
