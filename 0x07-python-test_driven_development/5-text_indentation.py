73a46cf1580e1f9b4e0bfcbd7dc5045a7c78e753
Adetwelve <niyero10@gmail.com>
2023-01-08 13:46:30 +0000

    task_5_test_update2

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,10 +15,12 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     i = 0
-    while i < len(text):
+    while (i < len(text)):
         print(text[i], end='')
         if text[i] in ['.', '?', ':']:
-            print()
-            print()
-            i += 1
+            print('\n')
+            j = i + 1
+            while text[j] == ' ':
+                i += 1
+                j += 1
         i += 1
