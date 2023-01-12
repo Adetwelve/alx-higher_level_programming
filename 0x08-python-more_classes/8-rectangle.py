18c95331e1f2ad34fdd989d02d5bc3e3453df8ac
Adetwelve <niyero10@gmail.com>
2023-01-12 23:57:09 +0000

    fix

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -87,4 +87,4 @@ class Rectangle():
             raise TypeError('rect_2 must be an istance of Rectangle')
         if rect_1.area() >= rect_2.area():
             return rect_1
-        rect_2
+        return rect_2
