1d784c851937e3f2461ff30d971c30f26b51fd94
Adetwelve <niyero10@gmail.com>
2023-01-15 18:43:29 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class: Rectangle subclass Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines the subclass: Square """
+
+    def __init__(self, size):
+        """Initialize a square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """computes the are of the square"""
+
+        return pow(self.__size, 2)
+
+    def __str__(self):
+        """retrn print() and str() of rep of Square"""
+
+        string = '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        return string
