9192483013f98f35eb0c2561e40de644eab5c62c
Adetwelve <niyero10@gmail.com>
2023-01-16 22:48:32 +0000

    Task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyton3
+"""creates a class: 'MyInt'"""
+
+
+class MyInt(int):
+    """Returns the inverse of '==' and '!=' oprators"""
+
+    def __eq__(self, other):
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
