5759d82df0fc799cb257c323173387aec633c13c
Adetwelve <niyero10@gmail.com>
2023-01-22 21:22:44 +0000

    style fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module to create function: class_to_json"""
 
+
 def class_to_json(obj):
     """returns the dictionary description with simple data
     structure (list, dictionary, string, integer and boolean)
