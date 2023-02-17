ea663edd04e03bf18ef143e3236809d1af8186e7
Adetwelve <niyero10@gmail.com>
2023-02-17 02:43:32 +0000

    0x0E

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+--a script that creates the MySQL server user user_0d_1
+--user_0d_1 should have all privileges on your MySQL server
+--user_0d_1 password should be set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS user_0d_1
+GRANT ALL PRIVELEGE ON *.* TO 'user_0d_1'@'localhost';
