fc73ff592eb7fa9c4e0091d72fac79cfea34ad13
Adetwelve <niyero10@gmail.com>
2023-03-17 07:40:03 +0000

    replace

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let sq = '';
+      for (let j = 0; j < this.width; j++) {
+        sq += 'X';
+      }
+      console.log(sq);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+module.exports = Rectangle;
