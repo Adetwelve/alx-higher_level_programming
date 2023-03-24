4ffb68827140dafc4de9c59f994cc7280a308d1c
Adetwelve <niyero10@gmail.com>
2023-03-24 13:21:11 +0000

    orm

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Lists all State objects from database hbtn_0e_6_us
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import sys
+
+if __name__ == '__main__':
+    # create link to the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # create all the table associated with Base metadata
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query data of all element in the state
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    # print query
+    if state:
+        print(state.id)
+    print("Not found")
+
+    session.close()
