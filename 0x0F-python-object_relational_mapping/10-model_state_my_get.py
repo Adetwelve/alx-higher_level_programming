eccb10cf5685e3627a750ba3826c0480cace01e7
Adetwelve <niyero10@gmail.com>
2023-03-24 13:30:17 +0000

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that prints the State object with the name passed as 
+"""a script that prints the State object with the name passed as
    argument from the database hbtn_0e_6_usa
 """
 from model_state import Base, State
